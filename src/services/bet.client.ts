bb537ec04c2d8545142e6dfc42fe96396958d9ce
François Jolain <4466185+FrancoisJ@users.noreply.github.com>
2024-03-30 11:12:07 +0100

    add cache to NTV API

--- src/services/bet.client.ts
+++ src/services/bet.client.ts
@@ -3,6 +3,7 @@ import {Account} from "../domain/account";
 import {Round} from "../domain/round";
 import {Bet} from "../domain/bet";
 import axios from "axios";
+import AsyncLock from "async-lock";
 
 /*
 [{"address":"1JDi2dx4gYVde7K9YFsvSva8Md2tt4BeRBHvpueJ5FsjX",
@@ -27,12 +28,33 @@ export type BetDTO = {
 }
 
 export class BetClient {
+    private static CACHE_MS = 1000;
+    private lastFetch = new Map<string, number>;
+    private caches = new Map<string, BetDTO[]>();
+    private lock = new AsyncLock();
+
     constructor(private readonly host: string) {}
 
     async getAllPlayerBets(game: Game, account: Account): Promise<BetDTO[]> {
-        const res = await axios.get<BetDTO[]>(`${this.host}/allround/${game.contract.id}/${account.address}`)
-            .then(res => res.status == 200 ? res.data : [])
-            .catch(res => []);
-        return res;
+        return this.lock.acquire('GETBETS', async () => {
+            const now = Date.now();
+
+            // use cache
+            if (now < (this.lastFetch.get(game.id) ?? 0) + BetClient.CACHE_MS) {
+                return this.caches.get(game.id)!;
+            }
+
+            // fetch
+            const bets: BetDTO[] = await this.fetch(game, account).catch(() => []);
+
+            this.lastFetch.set(game.id, now);
+            this.caches.set(game.id, bets);
+            return bets;
+        });
+    }
+
+    private async fetch(game: Game, account: Account): Promise<BetDTO[]> {
+        return  axios.get<BetDTO[]>(`${this.host}/allround/${game.contract.id}/${account.address}`)
+            .then(res => res.status == 200 ? res.data : []);
     }
 }
