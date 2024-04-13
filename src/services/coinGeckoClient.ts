4292e56b0ca125871e6ffec797fbc4951b70fcd7
François Jolain <4466185+FrancoisJ@users.noreply.github.com>
2024-04-13 15:38:13 +0200

    fix price cache bug

--- src/services/coinGeckoClient.ts
+++ src/services/coinGeckoClient.ts
@@ -2,25 +2,27 @@ import axios from "axios";
 
 export class CoinGeckoClient {
     private static readonly ONE_MINUTE = 60*1000;
-    private lastValue: number = 0;
-    private lastDate: number = 0;
+    private lastValues: Map<string, number> = new Map();
+    private lastDates: Map<string, number> = new Map();
 
     constructor() {
     }
 
     async getPriceAlph(symbol: string): Promise<number> {
-        if ((Date.now() - this.lastDate) > CoinGeckoClient.ONE_MINUTE) {
+        if ((Date.now() - (this.lastDates.get(symbol) ?? 0)) > CoinGeckoClient.ONE_MINUTE) {
             try {
                 const res = await axios.get(
                     `https://api.coingecko.com/api/v3/simple/price?ids=${symbol}&vs_currencies=usd`,
                     {validateStatus: (status) => true},
                 );
-                this.lastValue = res.status === 200 ? res.data[symbol].usd : this.lastValue;
+                if (res.status === 200) {
+                    this.lastValues.set(symbol, res.data[symbol].usd);
+                }
             } catch (e) {} finally {
-                this.lastDate = Date.now();
+                this.lastDates.set(symbol, Date.now());
             }
         }
 
-        return this.lastValue ?? 0;
+        return this.lastValues.get(symbol) ?? 0;
     }
 }
